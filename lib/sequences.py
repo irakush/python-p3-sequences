4205355b4a24b2d2fc9a55ae8f1b6d87dbf5a331
Igor Rakush <irakush@aol.com>
2024-01-10 19:47:12 -0500

    pass the test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib_sequence = [
+            0,
+            1,
+        ]
+
+        while len(fib_sequence) < length:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+
+        print(fib_sequence)
+
+
+print_fibonacci(0)
